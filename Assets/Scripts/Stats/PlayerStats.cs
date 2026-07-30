36667107a76ab48d2e3c48c8dfd10f3339bc17fc
Sehwan Jang <sutrasam7@gmail.com>
2026-07-29 17:16:09 -0700

    Revert "Merge branch 'main' of https://github.com/bung1r/Bodyguard-LLC"
    This reverts commit 888fca2050b33ee53f572fe48d1c80a377e4074a, reversing
    changes made to e476c561cbaa94fd0ba3649abee478d7dda3564e.

--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -5,14 +5,5 @@ using UnityEngine.Events;
 [Serializable]
 public class PlayerStats
 {
-    public bool playerBool = false;
-}
-
-[Serializable]
-public class RuntimePlayerStats : PlayerStats
-{
-    public RuntimePlayerStats(PlayerStats playerStats)
-    {
-        playerBool = playerStats.playerBool;
-    }
+    
 }
